805d608bb0b3d20480897362705c557bb810b095
Younsoo Tom Lim <tomandlim@gmail.com>
2021-11-24 12:14:47 +0900

    Add: 핀 저장 기능 활성화

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,11 @@
-const BASE_URL = 'http://15.164.170.124:8000';
+const BASE_URL = 'http://10.58.2.217:8000';
 
 export const API = {
   signUp: `${BASE_URL}/users/signup`,
+  pin: `${BASE_URL}/boards/pin`,
   baseUrl: `${BASE_URL}`,
   token:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eypZCI6NX0.jugJgM3JP9XFInnwQJbQt02wCRW_aUnWnv5HWNC0X_g',
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NTJ9.l3OPSN-L-aIIOqKFUUVkaCYlctGcafF5SQkK3PSAHow',
 };
 
 export const MockUp = {
--- src/components/Icons/SavePinIcon.js
+++ src/components/Icons/SavePinIcon.js
@@ -1,11 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { API } from '../../Config';
 
-const SavePinIcon = () => {
+const SavePinIcon = ({ pinId }) => {
   const [isSave, setIsSave] = useState(false);
 
-  const savePin = () => {
+  useEffect(() => {
+    fetch(API.pin, {
+      method: 'GET',
+      headers: { Authorization: API.token },
+    })
+      .then(res => res.json())
+      .then(result => {
+        result.pined_boards.map(pin =>
+          pin.id === pinId ? setIsSave(true) : setIsSave(false)
+        );
+        if (result.message === 'CREATE_SUCCESS') alert('핀이 저장되었습니다!');
+      });
+  }, [pinId]);
+
+  const savePin = e => {
+    e.stopPropagation();
     setIsSave(!isSave);
+
+    fetch(API.pin, {
+      method: 'POST',
+      headers: { Authorization: API.token },
+      body: JSON.stringify({
+        board_id: pinId,
+      }),
+    })
+      .then(res => res.json())
+      .then(result => {
+        if (result.message === 'CREATE_SUCCESS') alert('핀이 저장되었습니다!');
+        if (result.message === 'DELETE_SUCCESS') alert('핀이 삭제되었습니다!');
+      });
   };
 
   return <Save onClick={savePin}>{isSave ? '저장됨' : '저장'}</Save>;
